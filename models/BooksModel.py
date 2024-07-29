24cd2669cd1a8c76956b194952a2f20a80a3d663
Lynn Poh <pohwlynn@gmail.com>
2024-07-29 16:02:23 +0200

    Create book added + test

--- models/BooksModel.py
+++ models/BooksModel.py
@@ -1,1 +1,23 @@
-print('hi')
+from config.DBConnection import DBConnection
+from datetime import date
+
+
+class BooksModel:
+    def __init__(self):
+        self.db = DBConnection()
+
+    def create_book(self, book_data):
+        try:
+            query = "INSERT INTO books(stock, isbn13, author, original_publication_year, title, summary, genre_id, availability, best_seller) VALUES(%s, %s, %s, %s, %s, %s, %s, %s, %s)"
+            params = (book_data.get("stock", None),
+                      book_data.get("isbn13"),
+                      book_data.get("author"),
+                      book_data.get("original_publication_year", None),
+                      book_data.get("title"),
+                      book_data.get("summary", None),
+                      book_data.get("genre_id", None),
+                      book_data.get("availability", True),
+                      book_data.get("best_seller", False))
+            return self.db.execute_query(query, params)
+        except Exception as e:
+            print(f"Error: {e}")
--- src/controllers/BooksController.py
+++ src/controllers/BooksController.py
@@ -1,1 +1,13 @@
-prit('hi')
+from models.BooksModel import BooksModel
+
+
+class BooksController:
+    def __init__(self):
+        self.books_model = BooksModel()
+    def add_book(self, book_data):
+        try:
+            self.books_model.create_book(book_data)
+            print(f"Book added: {book_data["title"]}")
+            return dict(status_code = 200, message = "Book added successfully")
+        except Exception as e:
+            print (f"Error: {e}")
