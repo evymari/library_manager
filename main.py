7e14aad553f2570c6515d0bfe9ab517a9bf11343
Lynn Poh <pohwlynn@gmail.com>
2024-07-29 16:02:23 +0200

    Create book added + test

--- main.py
+++ main.py
@@ -1,5 +1,11 @@
 from config.DBConnection import DBConnection
+from src.controllers.BooksController import BooksController
 
-myConnection=DBConnection()
-print(myConnection.execute_query("SELECT * FROM books"))
+
+
+
+
+
+"""myConnection=DBConnection()
+print(myConnection.execute_query("SELECT * FROM books"))"""
 
