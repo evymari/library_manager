ec87ef203c6c6a74bd5c8bc05440a12fb50b0bb3
Lynn Poh <pohwlynn@gmail.com>
2024-07-31 01:10:17 +0200

    tests created for CREATE book

--- main.py
+++ main.py
@@ -2,14 +2,19 @@ from config.DBConnection import DBConnection
 from src.controllers.BooksController import BooksController
 
 book = BooksController()
+
+print(book.books_model.get_book_by_isbn("9780142412084")[0][0])
+
+
+"""
 book_data = {
-    "isbn13":"9780141365467",
-    "author":"Roald Dahl",
-    "title":"Danny the Champion of the World"
-    }
+    "isbn13": "9780141365467",
+    "author": "Roald Dahl",
+    "title": "Danny the Champion of the World"
+}
 
 register_book = book.add_book(book_data)
-print(register_book)
+print(register_book)"""
 
 """myConnection=DBConnection()
 print(myConnection.execute_query("SELECT * FROM books"))"""
--- src/controllers/BooksController.py
+++ src/controllers/BooksController.py
@@ -7,22 +7,23 @@ class BooksController:
 
     def add_book(self, book_data):
         try:
-            existing_book = self.books_model.get_book_by_isbn(book_data.get("isbn13"))
+            if self.books_model.book_data_validator(book_data):
+                print("Valid data")
+                existing_book = self.books_model.get_book_by_isbn(book_data.get("isbn13"))
 
-            if existing_book:
-                self.books_model.update_stock(book_data.get("isbn13"))
-                print(
-                    f"ISBN {book_data["isbn13"]} found. Title: {book_data["title"]} Author:{book_data["author"]}. Stock updated.")
-                return dict(status_code=200, message="Book stock updated successfully")
-            else:
-                book_data["stock"] = 1
-                new_book_id = self.books_model.create_book(book_data)
-                if new_book_id is not None:
-                    print(f"Book added: {book_data["title"]} Book ID: {new_book_id}")
-                    return dict(status_code=200, message="Book added successfully")
+                if existing_book:
+                    self.books_model.update_stock(book_data.get("isbn13"))
+                    print(f"ISBN {book_data["isbn13"]} found. Title: {book_data["title"]} Author:{book_data["author"]}. Stock updated.")
+                    return dict(status_code=200, message="Book stock updated successfully")
                 else:
-                    print("Failed to retrieve the new book ID.")
-                    return dict(status_code=500, message="Failed to add book.")
+                    book_data["stock"] = 1
+                    new_book_id = self.books_model.create_book(book_data)
+                    if new_book_id is not None:
+                        print(f"Book added: {book_data["title"]} Book ID: {new_book_id}")
+                        return dict(status_code=200, message="Book added successfully")
+                    else:
+                        print("Failed to retrieve the new book ID.")
+                        return dict(status_code=500, message="Failed to add book.")
         except Exception as e:
             print(f"Error: {e}")
             return dict(status_code=500, message=f"Error adding book: {e}")
