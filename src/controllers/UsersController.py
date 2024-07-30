9a905115f1a6330626666630db6c6f9e356cbf63
Lynn Poh <pohwlynn@gmail.com>
2024-07-30 13:46:20 +0200

    return everything that was lost

---
+++ src/controllers/UsersController.py
@@ -0,0 +1,48 @@
+from models.UsersModel import UsersModel
+import logging
+
+
+class UsersController:
+    def __init__(self):
+        self.user_model = UsersModel()
+        logging.basicConfig(level=logging.INFO)
+
+    def update_user(self, user_id, user_data):
+        try:
+            if self.data_validator(user_data):
+                result = self.user_model.update_user(user_id, user_data)
+                logging.info(f"Update result: {result}")
+                if not result:
+                    raise ValueError("Error updating user")
+                else:
+                    return {"status_code": 200, "message": "User updated successfully"}
+        except ValueError as ve:
+            logging.error(f"Value Error: {ve}")
+            return {"status_code": 500, "message": str(ve)}
+        except Exception as e:
+            logging.error(f"Error: {e}")
+            return {"status_code": 400, "message": "Error updating user"}
+
+
+
+    def data_validator(self, data):
+        expected_types = {
+            "dni": str,
+            "name": str,
+            "surname": str,
+            "email": str,
+            "phone": str,
+            "password": str,
+            "address": str,
+            "status": str,
+            "current_loans": int,
+            "max_loans": int
+        }
+        for key, value in data.items():
+            if key in expected_types:
+                if not isinstance(value, expected_types[key]):
+                    raise TypeError(
+                        f"Invalid type for {key}. Expected {expected_types[key].__name__}, got {type(value).__name__}.")
+            else:
+                raise KeyError(f"Unexpected key {key} found in data.")
+        return True
---
+++ tests/test_BooksController.py
@@ -0,0 +1,18 @@
+import pytest
+from src.controllers.BooksController import BooksController
+
+books_controller = BooksController()
+
+def test_add_book():
+
+    """
+    Given User introduces book data
+    When add book function is called
+    Then a book is added
+    And status 200 is returned
+    """
+    book_data = {"stock": None, "isbn13":"9780000222", 'author': "Jim Carrey", "original_publication_year": 1987, "title": "Who Are You", "summary": None, "genre_id": None, "availability": None, "best_seller": None}
+
+    result = books_controller.add_book(book_data)
+    expected_result = {"status_code": 200, "message": "Book added successfully"}
+    assert result["status_code"] == expected_result["status_code"]
