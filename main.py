efe0bac4f2b621c43f7ef72670a1c08433d8f237
evymari <144184061+evymari@users.noreply.github.com>
2024-08-01 23:52:38 +0200

    read_consultation_search_Books

--- main.py
+++ main.py
@@ -2,14 +2,14 @@ from config.DBConnection import DBConnection
 from src.controllers.BooksController import BooksController
 from models.GenresModel import GenresModel
 
-Book = BooksController()
+"""Book = BooksController()
 search_criteria= {
     "author":"Markus Zusak",
     "title":"The Messenger",
     "genre_id":None
 }
 
-print(Book.search_books(search_criteria))
+print(Book.search_books(search_criteria))"""
 
 
 """myConnection=DBConnection()
