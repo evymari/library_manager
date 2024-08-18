12f4242a1861f9017d4e2ac49ceca16cca27518c
evymari <144184061+evymari@users.noreply.github.com>
2024-08-19 00:11:34 +0200

    test_control_error_approved_book_search_done

--- tests/tests_books_controller/test_books_controller_read.py
+++ tests/tests_books_controller/test_books_controller_read.py
@@ -1,5 +1,6 @@
 import pytest
 
+
 def test_search_books_by_title(mock_books_controller_with_model):
     """
     Given search criteria title,
@@ -18,14 +19,14 @@ def test_search_books_by_title(mock_books_controller_with_model):
 
     mock_books_model.search_books.return_value = expected_books
     result = books_controller.search_books(search_criteria)
-    mock_books_model.search_books.assert_called_with(None, "Under the Dome", None, None ,None, None, None, None)
+
+    mock_books_model.search_books.assert_called_with(None, "Under the Dome", None, None, None, None, None, None)
 
     assert result["status_code"] == 200
     assert result["books"] == expected_books
 
 
 def test_search_books_by_author(mock_books_controller_with_model):
-
     """
        Given search criteria author,
       When search_books function is called
@@ -43,7 +44,8 @@ def test_search_books_by_author(mock_books_controller_with_model):
 
     mock_books_model.search_books.return_value = expected_books
     result = books_controller.search_books(search_criteria)
-    mock_books_model.search_books.assert_called_with("Suzanne Collins", None, None, None ,None, None, None, None)
+
+    mock_books_model.search_books.assert_called_with("Suzanne Collins", None, None, None, None, None, None, None)
 
     assert result["status_code"] == 200
     assert result["books"] == expected_books
@@ -72,4 +74,47 @@ def test_search_books_by_isbn13(mock_books_controller_with_model):
     assert result["books"] == expected_books
 
 
+def test_search_books_fail_missing_by_title(mock_books_controller_with_model):
+    """
+    Given search criteria,
+    When search_books function is called and an error occurs,
+    Then an error message is returned
+     And status 5
 
+     And status 500 is returned
+     """
+    books_controller, mock_books_model = mock_books_controller_with_model
+    # Mocked search criteria
+    search_criteria = {"title": "Non-existent Book"}
+
+    expected_books = [
+        {"book_id": 123, "title": "Under the Dome", "author": "Stephen King"},
+        {"book_id": 456, "title": "Under the Dome", "author": "Another Author"}
+    ]
+
+    mock_books_model.search_books.side_effect = Exception("Database error")
+    result = books_controller.search_books(search_criteria)
+    mock_books_model.search_books.assert_called_with(None, "Non-existent Book", None, None, None, None, None, None)
+
+    assert result["status_code"] == 500
+    assert "Error searching book" in result["message"]
+
+
+def test_search_books_by_author_error(mock_books_controller_with_model):
+    """
+    Given search criteria author,
+    When search_books function is called and an error occurs,
+    Then an error message is returned
+    And status 500 is returned
+    """
+    books_controller, mock_books_model = mock_books_controller_with_model
+    search_criteria = {"author": "Suzanne Collins"}
+
+    mock_books_model.search_books.side_effect = Exception("Database error")
+
+    result = books_controller.search_books(search_criteria)
+
+    mock_books_model.search_books.assert_called_with("Suzanne Collins", None, None, None, None, None, None, None)
+
+    assert result["status_code"] == 500
+    assert "Error searching book" in result["message"]
